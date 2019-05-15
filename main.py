dd0b4315436cc2687ced69d0747d13934700637b
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-05-15 13:35:19 +0430

    Generalized version

--- main.py
+++ main.py
@@ -66,88 +66,9 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     return mini_batches
 
 
-
-
 def trainFunction(X_train, Y_train, learning_rate = 0.0001,
-          num_epochs = 1500, minibatch_size = 32, print_cost = True):
-
-    ops.reset_default_graph()                         # to be able to rerun the model without overwriting tf variables
-    tf.set_random_seed(1)                             # to keep consistent results
-    seed = 3                    
-    
-    m = Y_train.shape[1]
-    n_x = X_train.shape[0]
-    n_y = Y_train.shape[0]
-
-    X = tf.placeholder(shape=[n_x, None] ,dtype=tf.float32, name='X')
-    Y = tf.placeholder(shape=[n_y, None] ,dtype=tf.float32, name='Y')
-    
-    costs = [] 
-
-    # parameters = initialize_parameters()
-    tf.set_random_seed(1)
-    b1 = tf.get_variable("b1", [1, 1], initializer = tf.zeros_initializer())
-    # using Xavier Initialization for weights
-    W1 = tf.get_variable("W1", [1, n_x], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-
-    Z1 = tf.add(tf.matmul(W1, X) , b1)
-
-    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = Z1,  labels = Y))
-
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-    # optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
-    
-    init = tf.global_variables_initializer() # Initialize all the variables
-
-    with tf.Session() as session:
-        session.run(init)
-        for i in range(num_epochs):
-            seed = seed + 1
-            epoch_cost = 0.
-            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
-            n_minibatches = np.floor(m / minibatch_size)
-            for minibatch in minibatches:
-                (minibatch_X, minibatch_Y) = minibatch
-                _ , minibatch_cost = session.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
-                epoch_cost += minibatch_cost / n_minibatches
-                # epoch_cost += minibatch_cost / minibatch_cost.shape[1]
-
-            
-            if print_cost == True:
-                if i % 100 == 0:
-                    print ("Cost after epoch %i: %f" % (i, epoch_cost))
-                if i % 5 == 0:
-                    costs.append(epoch_cost)
-            
-        
-        # plot the cost
-        if print_cost == True:
-            plt.plot(np.squeeze(costs))
-            plt.ylabel('cost')
-            plt.xlabel('iterations (per tens)')
-            plt.title("Learning rate =" + str(learning_rate))
-            plt.show()
-
-
-        parameters = {
-            "W1": W1,
-            "b1": b1
-        }
-        parameters = session.run(parameters)
-
-        print ("Parameters have been trained!")
-        correct_prediction = tf.equal(tf.argmax(Z1), tf.argmax(Y))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-        print ("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
-        # print ("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
-
-    return parameters
-
-
-
-def trainFunction2(X_train, Y_train, learning_rate = 0.0001,
-          num_epochs = 1500, minibatch_size = 32, print_cost = True):
+          num_epochs = 1500, minibatch_size = 32, print_cost = True, n_l=1):
+    # n_l = number of hidden layer
 
     ops.reset_default_graph()                         # to be able to rerun the model without overwriting tf variables
     tf.set_random_seed(1)                             # to keep consistent results
@@ -165,30 +86,34 @@ def trainFunction2(X_train, Y_train, learning_rate = 0.0001,
     # parameters = initialize_parameters()
     tf.set_random_seed(1)
 
-    n1 = 32
-    n2 = 32
-    n3 = 16
-    b1 = tf.get_variable("b1", [n1, 1], initializer = tf.zeros_initializer())
-    b2 = tf.get_variable("b2", [n2, 1], initializer = tf.zeros_initializer())
-    b3 = tf.get_variable("b3", [n3, 1], initializer = tf.zeros_initializer())
-    b4 = tf.get_variable("b4", [1, 1], initializer = tf.zeros_initializer())
+    # define n1 to n(n_l - 1) here
+    n = {}
+    n["0"] = n_x
+    n["1"] = 32
+    n["2"] = 32
+    n["3"] = 16
+
+    
+    parameters = {}
+    for i in range(1, n_l):
+        parameters["b" + str(i)] = tf.get_variable("b" + str(i), [n[str(i)], 1], initializer = tf.zeros_initializer())
+    parameters["b" + str(n_l)] = tf.get_variable("b" + str(n_l), [1, 1], initializer = tf.zeros_initializer())
+
+
     # using Xavier Initialization for weights
-    W1 = tf.get_variable("W1", [n1, n_x], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-    W2 = tf.get_variable("W2", [n2, n1], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-    W3 = tf.get_variable("W3", [n3, n2], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-    W4 = tf.get_variable("W4", [1, n3], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    for i in range(1, n_l):
+        parameters["W" + str(i)] = tf.get_variable("W" + str(i), [n[str(i)], n[str(i-1)]], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    parameters["W" + str(n_l)] = tf.get_variable("W" + str(n_l), [1, n[str(n_l - 1)]], initializer = tf.zeros_initializer())
 
+    A = {}
+    Z = {}
+    A["0"] = X
+    for i in range(1, n_l):
+        Z[str(i)] = tf.add(tf.matmul(parameters["W" + str(i)], A[str(i - 1)]) , parameters["b" + str(i)])
+        A[str(i)] = tf.nn.relu(Z[str(i)])     
+    Z[str(n_l)] = tf.add(tf.matmul(parameters["W" + str(n_l)], A[str(n_l - 1)]) , parameters["b" + str(n_l)])
 
-    # Z3 = forward_propagation(X, parameters)
-    Z1 = tf.add(tf.matmul(W1, X) , b1)
-    A1 = tf.nn.relu(Z1)             
-    Z2 = tf.add(tf.matmul(W2, A1) , b2)
-    A2 = tf.nn.relu(Z2)   
-    Z3 = tf.add(tf.matmul(W3, A2) , b3)
-    A3 = tf.nn.relu(Z3)   
-    Z4 = tf.add(tf.matmul(W4, A3) , b4)
-
-    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = Z4,  labels = Y))
+    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = Z[str(n_l)],  labels = Y))
 
 
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
@@ -226,21 +151,10 @@ def trainFunction2(X_train, Y_train, learning_rate = 0.0001,
             plt.title("Learning rate =" + str(learning_rate))
             plt.show()
 
-
-        parameters = {
-            "W1": W1,
-            "b1": b1,
-            "W2": W2,
-            "b2": b2,
-            "W3": W3,
-            "b3": b3,
-            "W4": W4,
-            "b4": b4
-        }
         parameters = session.run(parameters)
 
         print ("Parameters have been trained!")
-        correct_prediction = tf.equal(tf.argmax(Z1), tf.argmax(Y))
+        correct_prediction = tf.equal(tf.argmax(Z[str(n_l)]), tf.argmax(Y))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
         print ("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
         # print ("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
@@ -372,52 +286,21 @@ m = Y_train.shape[1]
 
 
 # TODO TODO TODO TODO TODO TODO TODO RUN MODEL HERE TODO TODO TODO TODO TODO TODO TODO TODO TODO
-parameters = trainFunction2(X_train, Y_train, 0.008, 10000, m, True)
-
-
-def pred(parameters, X, Y_train):
-
-    W1 = parameters['W1']
-    b1 = parameters['b1']
-
-
-    Y = np.dot(W1, X) + b1
-    Y = sigmoid(Y)[0]
-    Y = np.around(Y)
-    Y_train = Y_train[0]
-    Y_train = Y_train.astype(int)
-    # print(Y[0:5])
-    # print(Y_train[0:5])
-
-    g = np.subtract(Y, Y_train)
-    g = abs(g)
-    print(np.sum(g))
-    print("Train Accuracy = " + str(1 - (np.sum(g)) / m))
-    # print(parameters['W1'])
-    # print(parameters['b1'])
+parameters = trainFunction(X_train, Y_train, 0.008, 5000, m, True, 4)
 
 
 
-
-def pred2(parameters, X, Y_train):
-
-    W1 = parameters['W1']
-    b1 = parameters['b1']
-    W2 = parameters['W2']
-    b2 = parameters['b2']
-    W3 = parameters['W3']
-    b3 = parameters['b3']
-    W4 = parameters['W4']
-    b4 = parameters['b4']
-
-    Z1 = np.dot(W1, X) + b1
-    A1 = ReLu(Z1)         
-    Z2 = np.dot(W2, A1) + b2
-    A2 = ReLu(Z2) 
-    Z3 = np.dot(W3, A2) + b3
-    A3 = ReLu(Z3) 
-    Z4 = np.dot(W4, A3) + b4
-    Y = sigmoid(Z4)[0]
+def pred(parameters, X, Y_train, n_l = 1):
+   
+    A = {}
+    Z = {}
+    A["0"] = X
+    for i in range(1, n_l):
+        Z[str(i)] = np.dot(parameters["W" + str(i)], A[str(i - 1)]) + parameters["b" + str(i)]  
+        A[str(i)] = ReLu(Z[str(i)])   
+    Z[str(n_l)] = np.dot(parameters["W" + str(n_l)], A[str(n_l - 1)]) + parameters["b" + str(n_l)]  
+    
+    Y = sigmoid(Z[str(n_l)])[0]
     Y = np.around(Y)
     Y_train = Y_train[0]
     Y_train = Y_train.astype(int)
@@ -431,7 +314,7 @@ def pred2(parameters, X, Y_train):
 
 
 # TODO TODO TODO TODO TODO TODO TODO PREDICT MODEL HERE TODO TODO TODO TODO TODO TODO TODO TODO TODO
-pred2(parameters, x, Y_train)
+pred(parameters, x, Y_train, 4)
 
 
 
