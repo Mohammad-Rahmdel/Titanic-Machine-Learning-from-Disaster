f08a8805d5f6d539c083b23ca7c351a6ae36ce92
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-05-15 15:35:18 +0430

    First Submission

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ def trainFunction(X_train, Y_train, learning_rate = 0.0001,
     seed = 3                    
     
     m = Y_train.shape[1]
+    print(m)
     n_x = X_train.shape[0]
     n_y = Y_train.shape[0]
 
@@ -90,8 +91,10 @@ def trainFunction(X_train, Y_train, learning_rate = 0.0001,
     n = {}
     n["0"] = n_x
     n["1"] = 32
-    n["2"] = 32
-    n["3"] = 16
+    n["2"] = 64
+    n["3"] = 128
+    n["4"] = 64
+    n["5"] = 32
 
     
     parameters = {}
@@ -167,111 +170,128 @@ def trainFunction(X_train, Y_train, learning_rate = 0.0001,
 
 
 import pandas as pd
-data = pd.read_csv("./datasets/train.csv")
-# data = data[0:864]
-# print(data.shape)
-# print(data.head())
-m = data.shape[0] #891
-y = np.array([data.loc[:,'Survived']])
 
-x1 = np.array(data.loc[:,'Pclass'])
-x2 = np.array(data.loc[:,'Sex'])
-x3 = np.array(data.loc[:,'Age'])  # has NAN
-x4 = np.array(data.loc[:,'SibSp'])
-x5 = np.array(data.loc[:,'Parch'])
-x6 = np.array(data.loc[:,'Fare'])
+def data_manipulation(dataset):
+    data = pd.read_csv("./datasets/" + dataset + ".csv")
+    # data = data[0:864]
+    # print(data.shape)
+    # print(data.head())
+    m = data.shape[0] #891
+    if dataset == "train":
+        y = np.array([data.loc[:,'Survived']])
 
-x7 = np.array(data.loc[:,'Name'])
-x8 = np.array(data.loc[:,'Embarked'])  # has NAN
+    x1 = np.array(data.loc[:,'Pclass'])
+    x2 = np.array(data.loc[:,'Sex'])
+    x3 = np.array(data.loc[:,'Age'])  # has NAN
+    x4 = np.array(data.loc[:,'SibSp'])
+    x5 = np.array(data.loc[:,'Parch'])
+    x6 = np.array(data.loc[:,'Fare'])
 
+    if dataset == "test":
+        f = pd.isnull(x6)
+        c = []
+        for i in range(len(f)) :
+            if f[i] == True:
+                c.append(i)
+        for i in c:
+            x6[i] = 0
 
-x1 = normalizer(x1)
-x4 = normalizer(x4)
-x5 = normalizer(x5)
-x6 = normalizer(x6)
-
-x2[x2 == 'male'] = 1
-x2[x2 == 'female'] = -1
-x2 = normalizer(x2)
-
-#solving unknown age problem
-#replacing NANs with mean of ages
-f = np.isnan(x3)
-cnt = 0
-for i in f:
-    if i == True:
-        cnt += 1
-x3 = np.nan_to_num(x3)
-mean = np.sum(x3) / (m - cnt)
-x3[x3 == 0] = mean
-x3 = normalizer(x3)
+    x7 = np.array(data.loc[:,'Name'])
+    x8 = np.array(data.loc[:,'Embarked'])  # has NAN
 
 
+    x1 = normalizer(x1)
+    x4 = normalizer(x4)
+    x5 = normalizer(x5)
+    x6 = normalizer(x6)
+
+    x2[x2 == 'male'] = 1
+    x2[x2 == 'female'] = -1
+    x2 = normalizer(x2)
+
+    #solving unknown age problem
+    #replacing NANs with mean of ages
+    f = np.isnan(x3)
+    cnt = 0
+    for i in f:
+        if i == True:
+            cnt += 1
+    x3 = np.nan_to_num(x3)
+    mean = np.sum(x3) / (m - cnt)
+    x3[x3 == 0] = mean
+    x3 = normalizer(x3)
+
+
+    x8[x8 == 'S'] = 1
+    x8[x8 == 'C'] = 2
+    x8[x8 == 'Q'] = 3
+
+    f = pd.isnull(x8)
+    c = []
+    for i in range(len(f)) :
+        if f[i] == True:
+            c.append(i)
+    for i in c:
+        x8[i] = 0
+    mean = np.sum(x8) / (m - len(c))
+    x8[x8 == 0] = mean
+    x8 = normalizer(x8)
 
 
 
-x8[x8 == 'S'] = 1
-x8[x8 == 'C'] = 2
-x8[x8 == 'Q'] = 3
-
-f = pd.isnull(x8)
-c = []
-for i in range(len(f)) :
-    if f[i] == True:
-        c.append(i)
-for i in c:
-    x8[i] = 0
-mean = np.sum(x8) / (m - len(c))
-x8[x8 == 0] = mean
-x8 = normalizer(x8)
 
 
+    for i in range(len(x7)):
+        if "Mr." in x7[i]:
+            x7[i] = 1
+        elif "Mrs." in x7[i]:
+            x7[i] = 2
+        elif "Miss." in x7[i]:
+            x7[i] = 3
+        elif "Master." in x7[i]:
+            x7[i] = 4
+        elif "Dr." in x7[i]:
+            x7[i] = 5
+        else :
+            # print(x7[i])
+            x7[i] = 6
+
+    x7 = normalizer(x7)
+
+    #x7 = Name{
+    # Mr.
+    # Mrs.
+    # Miss.
+    # Master.
+    # Dr.
+    # Lady. (Lucille Christiana Sutherland) ("Mrs Morgan")    #1
+    # Ms. Encarnacion                                         #1
+    # Sir. Cosmo Edmund ("Mr Morgan")                         #1
+    # Don.                                                    #1
+    # Rev.                                                    #6
+    # Mme.                                                    #1
+    # Ms.                                                     #1
+    # Major.                                                  #2
+    # Gordon.                                                 #2
+    # Mlle.                                                   #2
+    # Col.                                                    #2
+    # Capt.                                                   #1
+    # Countess.                                               #1
+    # Jonkheer.                                               #1
+    # }
 
 
-
-for i in range(len(x7)):
-    if "Mr." in x7[i]:
-        x7[i] = 1
-    elif "Mrs." in x7[i]:
-        x7[i] = 2
-    elif "Miss." in x7[i]:
-        x7[i] = 3
-    elif "Master." in x7[i]:
-        x7[i] = 4
-    elif "Dr." in x7[i]:
-        x7[i] = 5
+    # x = np.vstack((x1, x2, x3, x4, x5, x6))
+    x = np.vstack((x1, x2, x3, x4, x5, x6, x7, x8))
+    # print(x.shape)
+    if dataset == "train" :
+        return x, y
+    elif dataset == "test" :
+        return x
     else :
-        # print(x7[i])
-        x7[i] = 6
+        print("WRONG INPUT!")
 
-x7 = normalizer(x7)
-
-#x7 = Name{
-# Mr.
-# Mrs.
-# Miss.
-# Master.
-# Dr.
-# Lady. (Lucille Christiana Sutherland) ("Mrs Morgan")    #1
-# Ms. Encarnacion                                         #1
-# Sir. Cosmo Edmund ("Mr Morgan")                         #1
-# Don.                                                    #1
-# Rev.                                                    #6
-# Mme.                                                    #1
-# Ms.                                                     #1
-# Major.                                                  #2
-# Gordon.                                                 #2
-# Mlle.                                                   #2
-# Col.                                                    #2
-# Capt.                                                   #1
-# Countess.                                               #1
-# Jonkheer.                                               #1
-# }
-
-
-x = np.vstack((x1, x2, x3, x4, x5, x6))
-# x = np.vstack((x1, x2, x3, x4, x5, x6, x7, x8))
-# print(x.shape)
+x, y = data_manipulation("train")
 
 
 X_train = x
@@ -285,12 +305,13 @@ m = Y_train.shape[1]
 # print(data.shape)
 
 
+n_l = 6
 # TODO TODO TODO TODO TODO TODO TODO RUN MODEL HERE TODO TODO TODO TODO TODO TODO TODO TODO TODO
-parameters = trainFunction(X_train, Y_train, 0.008, 5000, m, True, 4)
+parameters = trainFunction(X_train, Y_train, 0.001, 10000, m, True, n_l)
 
 
 
-def pred(parameters, X, Y_train, n_l = 1):
+def train_prediction(parameters, X, Y_train, n_l = 1):
    
     A = {}
     Z = {}
@@ -314,7 +335,47 @@ def pred(parameters, X, Y_train, n_l = 1):
 
 
 # TODO TODO TODO TODO TODO TODO TODO PREDICT MODEL HERE TODO TODO TODO TODO TODO TODO TODO TODO TODO
-pred(parameters, x, Y_train, 4)
+train_prediction(parameters, x, Y_train, n_l)
+
+
+def output(parameters):
+
+    X_test = data_manipulation("test") 
+    W = {}
+    b = {}
+    size = int (len(parameters) / 2 ) + 1
+    n_l = size - 1
+    for i in range(1, size):
+        W[str(i)] = parameters["W" + str(i)]
+        b[str(i)] = parameters["b" + str(i)]
+
+    A = {}
+    Z = {}
+    A["0"] = X_test
+    for i in range(1, n_l):
+        Z[str(i)] = np.dot(parameters["W" + str(i)], A[str(i - 1)]) + parameters["b" + str(i)]  
+        A[str(i)] = ReLu(Z[str(i)])   
+    Z[str(n_l)] = np.dot(parameters["W" + str(n_l)], A[str(n_l - 1)]) + parameters["b" + str(n_l)] 
+
+    Y_hat = sigmoid(Z[str(n_l)])[0]
+    Y_hat = np.around((Y_hat))
+
+    # print(Y_hat.shape)
+    # print(Y_hat)
+
+
+    data = pd.read_csv("./datasets/test.csv")
+    id = np.array(data.loc[:,'PassengerId'])
+    output = np.stack((id, Y_hat))
+    # output = np.transpose(output)
+    output = output.astype(int)
+    # print(output)
+    df = pd.DataFrame({"PassengerId" : output[0], "Survived" : output[1]})
+    df.to_csv("foo.csv", index=False)
+    # np.savetxt("foo.csv", output, delimiter=",", header="PassengerId,B", comments="")
+    
+
+output(parameters)
 
 
 
@@ -334,4 +395,10 @@ n_x = 8
 n_x = 8
 learning rate = 0.008
 90%
+
+6layers
+n_x = 8
+learning rate = 0.008
+train accuracy = 96%
+test accuracy = 72%
 """
