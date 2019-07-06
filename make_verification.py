e27f089b50d62174d207baab8f8137988059c120
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-07-06 11:46:05 +0430

    making labeled test set for finding accuracy

---
+++ make_verification.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+test_df = pd.read_csv("./datasets/test.csv")
+unordered = pd.read_csv("./datasets/answer.csv")
+
+res = pd.DataFrame()
+res['PassengerId'] = test_df['PassengerId']
+res['Survived'] = ''
+
+
+for index, row in test_df.iterrows():
+    for index2,row2 in unordered.iterrows():
+        if (row2['name']==row['Name'] and row2['age']==row['Age']) or (row2['name']==row['Name'] and row2['ticket']==row['Ticket']):
+            # res.loc[res['PassengerId'] == row['PassengerId'], 'Survived'] = row2['survived']
+            break
+
+res.to_csv("./datasets/labeled_test_set.csv", index = False)
